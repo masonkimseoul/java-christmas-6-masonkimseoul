2aab99c68de91a5a99f7901098b9fde416b6c268
masonkimseoul <masonkimseoul@gmail.com>
2023-11-13 14:48:10 +0900

    feat : 주문 입력 검증 기능 일부 구현
    빈 문자열 입력 시 예외

--- src/main/java/christmas/Util/Validation.java
+++ src/main/java/christmas/Util/Validation.java
@@ -1,7 +1,8 @@
 package christmas.Util;
 
 public class Validation {
-    private static final String INPUT_TYPE_ERROR_MSG = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String INPUT_DATE_ERROR_MSG = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static final String INPUT_ORDERS_ERROR_MSG = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
 
     private static final int MIN_VALUE = 1;
     private static final int MAX_VALUE = 31;
@@ -10,13 +11,19 @@ public class Validation {
         try {
             return Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INPUT_TYPE_ERROR_MSG);
+            throw new IllegalArgumentException(INPUT_DATE_ERROR_MSG);
         }
     }
 
     private static void validateDateRange(int input) {
         if (input < MIN_VALUE || input > MAX_VALUE) {
-            throw new IllegalArgumentException(INPUT_TYPE_ERROR_MSG);
+            throw new IllegalArgumentException(INPUT_DATE_ERROR_MSG);
+        }
+    }
+
+    private static void validateFilledOrders(String input) {
+        if (input.isBlank()) {
+            throw new IllegalArgumentException(INPUT_ORDERS_ERROR_MSG);
         }
     }
 
@@ -24,5 +31,10 @@ public class Validation {
         int visitDate = parseInteger(input);
         validateDateRange(visitDate);
         return visitDate;
+    }
+
+    public static String validateOrders(String input) {
+        validateFilledOrders(input);
+        return input;
     }
 }
